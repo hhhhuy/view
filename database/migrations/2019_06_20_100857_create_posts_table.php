fb22a2aa5a9fb5eee15a2dc308e246dadc79b05a
vanlinh1211 <vanlang1211@gmail.com>
2019-06-20 17:28:30 +0700

    post

---
+++ database/migrations/2019_06_20_100857_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('species');
+            $table->string('address');
+            $table->integer('bedroom');
+            $table->integer('bathroom');
+            $table->string('description');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
